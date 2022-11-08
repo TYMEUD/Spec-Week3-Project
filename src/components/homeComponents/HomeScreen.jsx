762a9e80d5ce43c1debbf1f2495b4c50d7f3c209
Ty Menlove <tjmundecided@gmail.com>
2022-11-08 11:53:09 -0700

    added functionality to HomeScree.jsx and NewRecipeSreen.jsx

--- src/components/homeComponents/HomeScreen.jsx
+++ src/components/homeComponents/HomeScreen.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import AdBanner from './AdBanner'
+import RecipeContainer from './RecipeContainer'
+import axios from 'axios'
+import {useState, useEffect} from 'react'
 
-const HomeScreen = () => {  
+const HomeScreen = () => { 
+  const [recipes, setRecipes]= useState([])
+  const getRecipes = () => {
+      axios.get('https://recipes.devmountain.com/recipes')
+      .then (res => {
+        console.log(res.data)
+        setRecipes(res.data)
+      })
+  }
+  useEffect(getRecipes,[])
   return (
     <div>
       <AdBanner />
-      {/* Much code from Part 2 will be placed around here. Do your best! */}
+      <RecipeContainer recipes={recipes}/>
     </div>
   )
 }
--- src/components/newRecipeComponents/NewRecipeScreen.jsx
+++ src/components/newRecipeComponents/NewRecipeScreen.jsx
@@ -1,11 +1,80 @@
 import React from "react";
+import {useState, useEffect} from 'react';
+import axios from "axios";
 
 const NewRecipeScreen = () => {
+  const [ingredients, setIngredients]= useState([]);
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState("");
+
+  const addIngredient = () => {
+    setIngredients([...ingredients, { name, quantity }]);
+    setName("");
+    setQuantity("");
+};
+
+  const initialValues = {
+    type: "",
+    recipeName: "",
+    imageURL: "",
+    prepTime: "",
+    cookTime: "",
+    serves: "",
+    ingredients: [],
+    instructions: "",
+};
+
+const onSubmit = (values) => {
+  values.ingredients = ingredients;
+  console.log(values);
+};
   return (
     <section>
       <h1>Tell us about your Recipe!</h1>
-      {/* Here you will have a large form. Be prepared, part 4 will have you build this form in detail, and part 5 will have you style it. Do your best! */}
-    </section>
+      <Formik initialValues={initialValues} onSubmit={onSubmit}/>
+    {({ values, handleChange, handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+        <input 
+          placeholder="Title your Recipe!"
+          value={values.recipeName}
+          onChange={handleChange}
+          name="recipeName" type="text" />
+        <input type="text"
+           placeholder="Image"
+           value={values.imageURL}
+           onChange={handleChange}
+           name="imageURL"/>
+        <input type="text" 
+          placeholder="Prep time"
+          value={values.prepTime}
+          onChange={handleChange}
+          name="prepTime"/>
+        <input type="text" 
+          placeholder="Cook Time"
+          value={values.cookTime}
+          onChange={handleChange}
+          name="cookTime"/>
+        <input type="text" 
+          placeholder="Servings"
+          value={values.serves}
+          onChange={handleChange}
+          name="serves"/>
+        <input type="text" 
+          placeholder="Indgredients"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          name="ingredients"/>
+        <input type="text" 
+          placeholder="Quantity"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+          name="quantity"/>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <button>Cook</button>
+        <button>Bake</button>
+        <button>Drink</button>
+      </form>
+     </section>
   );
 };
 
